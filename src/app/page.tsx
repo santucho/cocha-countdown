2333012ef0d588701fd630336f40b5d8c3d89fbe
Santiago Goyeneche <11729146+santucho@users.noreply.github.com>
2023-06-16 00:03:35 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="stars"></div>
       <div className="lights">
-        <Countdown date={new Date("Jun 18, 2023 23:30:00")}/>
+        <Countdown date={new Date("Jun 19, 2023 02:30:00")}/>
       </div>
     </main>
   )
